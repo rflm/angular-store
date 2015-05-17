ce678dd40e2484aa38233cd7ae22bb48aeb562a4
rflm <rafalmularski@outlook.com>
2015-05-17 15:45:28 +0200

    Better reviews and faker.js added

--- app.js
+++ app.js
@@ -15,13 +15,13 @@
       return this.tab === checkTab;
     };
   });
-
+/*
   var items = [
     {
-      name: 'blah',
+      name: faker.lorem.words()[1],
       price: 2,
-      description: '...',
-      specifications: 'None yet',
+      description: faker.lorem.sentence(),
+      specifications: faker.lorem.sentence(),
       reviews: 'None yet',
     },
     {
@@ -32,4 +32,28 @@
       reviews: 'None yet',
     },
   ];
+*/
+  var items = []
+
+  for (var i = 0; i < 10; i++) {
+    items[i] = {
+      name: faker.lorem.words()[1],
+      price: (Math.random() * (100.99 - 0.01) + 0.01).toFixed(2),
+      description: faker.lorem.sentence(),
+      specifications: faker.lorem.sentence(),
+      reviews: sampleReviews(5)
+    };
+  };
+
+  function sampleReviews(number) {
+    var reviews = [];
+    for (var i = 0; i < number; i++) {
+      reviews[i] = {
+        stars: Math.floor(Math.random() * (5 - 1 + 1)) + 1,
+        body: faker.lorem.sentence(),
+        author: faker.internet.email()
+      };
+    };
+    return reviews;
+  };
 })();
